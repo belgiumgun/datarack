3823c4df370c09921b92aae396a82f0195176d4d
Belgium gun NitrousIO <belgiumgun@yahoo.com>
2013-10-19 07:23:26 +0000

    final Foundation., last push . Moving to Bootstrap V3 :)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -16,15 +16,18 @@ class WelcomeController < ApplicationController
   p params[:filerack]
 
   file_params= params.require(:filerack).permit(:datafile)
+    p file_params
+    
     @filerack = Filerack.create(file_params) do |u|
       u.user_id = current_user.id
       u.score = 0
     end
+    
     p current_user.id.class
     @allfiles = Filerack.where(:user_id =>current_user.id)
     p @allfiles.count
-    render "welcome/home"
-      
+   # render "welcome/home"
+      redirect_to :back  
  end
     
     def deletefile
