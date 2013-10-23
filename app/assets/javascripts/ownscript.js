4439c6685fb8823b9676d14834605404689eab76
Belgium gun NitrousIO <belgiumgun@yahoo.com>
2013-10-23 02:25:54 +0000

    heroku - sqlite gem commented

--- app/assets/javascripts/ownscript.js
+++ app/assets/javascripts/ownscript.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-  
+  alert("ok, ill close");
   $('#public_files_open').click(function(){
   alert("ok, ill close");
   });
   
   $('.file_delete').click(function(){
+    alert("???????????????");
      $(this).closest("tr").remove();
     $.post("/file/delete",{id: $(this).attr("id")})
   });
