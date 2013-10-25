3286c47ebc577bc76ac777dd0bafc3a977a2954c
Belgium gun NitrousIO <belgiumgun@yahoo.com>
2013-10-25 11:33:46 +0000

    heroku push -- multiple files upload basic

--- app/assets/javascripts/ownscript.js
+++ app/assets/javascripts/ownscript.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
- // alert("ok, ill close");
-  //$("#files_pannel").css("display:none");
+ 
+  var global_files;
+  
   $('#public_files_open').click(function(){
-  //alert("ok, ill close");
+  
   });
   
   $('.file_delete').click(function(){
-    //alert("???????????????");
      $(this).closest("tr").remove();
     $.post("/file/delete",{id: $(this).attr("id")})
   });
@@ -14,21 +14,25 @@ $(document).ready(function(){
 
   $("#file_uploader").change(function(){
    alert("wasup");
-  //  $("#file_list").append("<p>"+current_file.name+current_file.size+"</p><br/>");
   var temp_files=this.files;
     $("#files_pannel").css("display:inline");
     for (var i=0,current_file; current_file=temp_files[i];i++)
     {
-      //$("#body_table").append("<tr><td>"+current_file.name+"</td><button class='btn btn-sm'>delete</tr>");
-      $("#body_table").append("<tr><td>"+current_file.name+"</td>"+"<td><button class='btn btn-sm btn-danger'>Delete<span class='glyphicon glyphicon-remove'></span>"+"</button></td></tr>");
-      
-     
-    }
+      $("#body_table").append("<tr><td><div style='display:none' type='file'>"+current_file+"</div>"+current_file.name+"</td>"+"<td><div class='file_remove'><button class='btn btn-sm btn-danger'>Delete<span class='glyphicon glyphicon-remove'></span>"+"</button></div></td></tr>");
+     }
     
-    
+              $('.file_remove').click(function(){
+               // $("#file_uploader").files.pop("abc.txt");
+                 $(this).closest("tr").remove();
+              
+              }); 
     
   });
 
+ 
+
+  
+  
 
   
 });
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,25 +12,32 @@ class WelcomeController < ApplicationController
   
   
   def insertfile
-  
-  p params[:datafile]
+ 
   p params[:filerack]
-
-  file_params= params.require(:filerack).permit(:datafile)
-
-    p file_params
     
+ 
+   params["filerack"]["datafile"].each do |datafile|
+   
+    params["filerack"]["datafile"]= datafile
+    
+     file_params= params.require(:filerack).permit(:datafile)
+       
     @filerack = Filerack.create(file_params) do |u|
       u.user_id = current_user.id
       u.score = 0
     end
     
-    p current_user.id.class
+    end
+      
+   
     @allfiles = Filerack.where(:user_id =>current_user.id)
     p @allfiles.count
    # render "welcome/home"
       redirect_to :back  
  end
+    
+    
+    
     
     def deletefile
 
